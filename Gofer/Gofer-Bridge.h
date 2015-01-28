2fbb15e1842388c1d8580830e5f9be0d07532b8e
Ben Nicholas <ben@ben-nicholas.com>
2015-01-27 19:03:24 -0500

    Add bridging header and bring Stripe over to swift land

---
+++ Gofer/Gofer-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Gofer-Bridge.h
+//  Gofer
+//
+//  Created by Ben Nicholas on 1/27/15.
+//  Copyright (c) 2015 Ben Nicholas. All rights reserved.
+//
+
+#ifndef Gofer_Gofer_Bridge_h
+#define Gofer_Gofer_Bridge_h
+
+
+#endif
