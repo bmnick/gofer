926109cfa9679c4576c1e1f12c850eb6d27479fa
Ben Nicholas <ben@ben-nicholas.com>
2015-01-28 23:27:06 -0500

    Adding basic flow for the payment requests

--- ParseServer/cloud/main.js
+++ ParseServer/cloud/main.js
@@ -3,3 +3,46 @@ var Stripe = require('stripe');
 Parse.Config.get().then(function(config) {
   stripe.initialize(config.get('stripe_secret_key'));
 });
+
+// States for individual requests:
+var unpaidState = 0,
+    readyState = 1,
+    fulfilledState = 2,
+    cancelledState = 3,
+    errorState = 4;
+
+/*
+ * Create a CoffeeRequest, authorize payment through stripe, and push the request
+ * into a fulfillable state.
+ */
+ Parse.Cloud.define('requestCoffee', function(request, response) {
+   Parse.Cloud.useMasterKey();
+
+   var coffeeRequest;
+
+   Parse.Promise.as().then(function() {
+     // Create a new coffee request with the given information
+     var newRequest = new Parse.Object('CoffeeRequest');
+     newRequest.set('destinationAddress', request.params.destinationAddress);
+     newRequest.set('destinationGeo', request.params.destinationGeo);
+     newRequest.set('state', unpaidState);
+
+     return newRequest.save();
+
+   }).then(function(result) {
+     // Authorize the payment to stripe
+
+   }).then(function(result) {
+     // Move the request to a fulfillable state
+
+   }, function(error) {
+     // This is a global error handler
+     if (coffeeRequest) {
+       coffeeRequest.set('state', errorState);
+
+       coffeeRequest.save().then(function() {
+         response.error(error);
+       });
+     }
+   });
+ });
